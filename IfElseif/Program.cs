a8a9e60ab6324d889933dca46c85683ff3202db1
Fatih Topcu <fthtpu@gmail.com>
2021-10-04 19:04:14 +0300

    ifelseif

---
+++ IfElseif/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace IfElseif
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int time=DateTime.Now.Hour;
+            
+            if(time>=6 && time<11)
+                Console.WriteLine("gunaydin");
+            else  if(time<=18)
+                Console.WriteLine("iyi gunler");
+            else 
+                Console.WriteLine("iyi geceler");
+
+            string sonuc =time<=18 ? "iyi Gunler": "iyi geceler";
+            sonuc = time >= 6  && time < 11 ? "Gunaydin" : time <= 18 ? "iyi Gunler" : "iyi Geceler";
+            Console.WriteLine(sonuc);
+        }
+    }
+}
