64b030198b665c86b4d8655d1730675e70fd998d
Fatih Topcu <fthtpu@gmail.com>
2021-11-30 19:36:23 +0300

    soru 3

--- Odev-2/Koleksiyonlar-Soru-3/Program.cs
+++ Odev-2/Koleksiyonlar-Soru-3/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace Koleksiyonlar_Soru_3
 {
@@ -6,7 +8,23 @@ namespace Koleksiyonlar_Soru_3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Soru - 3: Klavyeden girilen cümle içerisindeki sesli harfleri bir dizi içerisinde saklayan 
+            //dizinin elemanlarını sıralayan programı yazınız.
+            // a, e, ı, i, o, ö, u, ü
+            ArrayList sesliHarfler = new ArrayList(){'a','e','ı','i','o','ö','u','ü'};
+            ArrayList girilenSesliHarfler = new ArrayList() ;
+            string girilen = Console.ReadLine();
+            for (int i = 0; i < girilen.Length; i++)
+            {
+                if(sesliHarfler.Contains(girilen[i])){
+                    girilenSesliHarfler.Add(girilen[i]);
+                }   
+            }
+            girilenSesliHarfler.Sort();
+            foreach (var item in girilenSesliHarfler)
+            {
+                System.Console.WriteLine(item);
+            }
         }
     }
 }
