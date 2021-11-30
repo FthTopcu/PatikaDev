b6f41e00489d0362b9fa8bb5efe5a1caa6070ed8
Fatih Topcu <fthtpu@gmail.com>
2021-11-30 19:19:34 +0300

    Soru 2

--- Odev-2/Koleksiyonlar-Soru-2/Program.cs
+++ Odev-2/Koleksiyonlar-Soru-2/Program.cs
@@ -6,7 +6,33 @@ namespace Koleksiyonlar_Soru_2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Soru - 2: Klavyeden girilen 20 adet sayının en büyük 3 tanesi ve en küçük 3 tanesi bulan, 
+            // her iki grubun kendi içerisinde ortalamalarını alan 
+            // ve bu ortalamaları ve ortalama toplamlarını console'a yazdıran programı yazınız. (Array sınıfını kullanarak yazınız.)
+            int[] dizi = new int[20];
+            int sayi;
+            double buyukOrt = 0;
+            double kucukOrt = 0;
+            for (int i = 0; i < 20; i++)
+            {
+                sayi = Convert.ToInt32(Console.ReadLine());
+                sayi = rnd.Next(0,50);
+                dizi[i]=sayi;
+            }
+            Array.Sort(dizi);
+            for (int i = 0; i < 3; i++)
+            {
+                kucukOrt +=dizi[i];
+            }
+            for (int i = dizi.Length-1; i >= dizi.Length -4 ; i--)
+            {
+                buyukOrt += dizi[i];
+            }
+            foreach (var item in dizi)
+            {
+                System.Console.WriteLine(item);
+            }
+            System.Console.WriteLine($"Buyuk ortalama = {buyukOrt} - Kucuk ortalama = {kucukOrt} - Ortalamaların toplamları = {buyukOrt+kucukOrt}");
         }
     }
 }
