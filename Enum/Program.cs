3d078fa8b60ff97a78f68644d46c04a57fdb1e08
Fatih Topcu <fthtpu@gmail.com>
2021-12-05 16:36:43 +0300

    enum

---
+++ Enum/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Enum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine(Gunler.Pazar);
+            System.Console.WriteLine((int)Gunler.Cumartesi);
+
+            int sicaklik = 25;
+            if (sicaklik <= (int)HavaDurumu.Normal)
+            {
+                System.Console.WriteLine("Dışarıya çıkmak için havanın ısınmasını bekle.");
+            }
+            else if (sicaklik >= (int)HavaDurumu.Sicak)
+                System.Console.WriteLine("sıcak bir gün");
+            else if (sicaklik >= (int)HavaDurumu.Normal && sicaklik < (int)HavaDurumu.Sicak)
+                System.Console.WriteLine("dışarı çıkalım");
+        }
+    }
+    enum Gunler
+    {
+        Pazartesi,
+        Sali,
+        Çarşamba,
+        Perşembe,
+        Cuma = 23,
+        Cumartesi,
+        Pazar
+    }
+    enum HavaDurumu
+    {
+        Soguk = 4,
+        Normal = 20,
+        Sicak = 25,
+        CokSicak = 35
+    }
+}
---
+++ Struct/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Struct
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dikdortgen dikdortgen = new Dikdortgen();
+            dikdortgen.KisaKenar = 3;
+            dikdortgen.UzunKenar = 4;
+
+            System.Console.WriteLine("Class Alan Hesabı : " + dikdortgen.AlanHesapla());
+
+            Dikdortgen_Struct dikdortgen_Struct = new Dikdortgen_Struct(3,4);
+            System.Console.WriteLine("Class Alan Hesabı : " + dikdortgen_Struct.AlanHesapla());
+
+        }
+    }
+    class Dikdortgen
+    {
+        public int KisaKenar;
+        public int UzunKenar;
+        public long AlanHesapla()
+        {
+            return this.KisaKenar * this.UzunKenar;
+        }
+    }
+    struct Dikdortgen_Struct
+    {
+     
+        public int KisaKenar;
+        public int UzunKenar;
+        public Dikdortgen_Struct(int kisaKenar,int uzunKenar)
+        {
+            KisaKenar = 3;
+            UzunKenar = 4;
+        }
+        public long AlanHesapla()
+        {
+            return this.KisaKenar * this.UzunKenar;
+        }
+    }
+}
